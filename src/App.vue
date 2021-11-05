95428b48ff571a4965f69fa0a652e8476d6939ff
Piotr Polanski <polanski.pio@gmail.com>
2021-11-05 23:49:22 +0900

    fetch data

--- src/App.vue
+++ src/App.vue
@@ -9,5 +9,26 @@ export default {
   components: {
     Header,
   },
+  data() {
+    return {
+      loading: true,
+      title: "Global",
+      dataDate: "",
+      state: {},
+      countires: [],
+      loadingImage: require(""),
+    };
+  },
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch("https://api.covid19api.com/summary");
+      const data = await res.json();
+      return data;
+    },
+  },
+  async created() {
+    const data = await this.fetchCovidData();
+    console.log(data);
+  },
 };
 </script>
